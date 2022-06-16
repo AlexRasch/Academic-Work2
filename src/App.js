e2ca3ff117a481616683cddc85c52fc730a39db8
Viktor <viktor.holm@awacademy.training>
2022-06-16 11:15:03 +0200

    hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState} from "react";
+
+function ShoppingList(props) {
+  //ToDo change to local storage
+  const shoppingList2 = props.list.map((x) => <li>{x}</li>);
+  return <ul>{shoppingList2}</ul>;
+}
+
+
+function Item(props) {
+  return (
+    <div className="item">
+      <img src={props.image} width="100" alt=""></img>
+      <p>
+        {props.name} {props.price}
+      </p>
+      <button className="addButton" onClick={() => props.handleAdd(props)}>
+        Add item
+      </button>
+      <button className="removeButton" onClick={() => props.handleRemove(props)}>
+        Remove item
+      </button>
+    </div>
+  );
+}
 
 function App() {
+  
+  const [shoppingList, setShoppingList] = useState([]);
+  function removeItem() {}
+  function addItem(item) {
+console.log(item)
+    // setShoppingList
+    // console.table(event.target);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ShoppingList list = {shoppingList} />
+      <Item
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/375px-Banana-Single.jpg"
+        name="banana"
+        price={30}
+        handleAdd = {addItem}
+        handleRemove = {removeItem}
+      />
+      {/* <Item
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/375px-Banana-Single.jpg"
+        name="banana"
+        price={30}
+      />
+      <Item
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/375px-Banana-Single.jpg"
+        name="banana"
+        price={30}
+      />
+      <Item
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/375px-Banana-Single.jpg"
+        name="potatis"
+        price={30}
+      /> */}
     </div>
   );
 }
